3cfcf0dd0f5cf7dfd461a3b1f60c075fcdc820cd
Prawal Sharma <prawal.sharma@successive.tech>
2022-01-13 23:08:06 +0530

    Updated the PROD server URL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://mypsreactapp.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
